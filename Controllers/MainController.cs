36b8216b869eeeb5e565728dd1180ff8afbe3da7
Shoro749 <romanbro749@gmail.com>
2024-09-15 17:26:19 +0300

    Commit 2

--- Controllers/MainController.cs
+++ Controllers/MainController.cs
@@ -1,13 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebAspDBeaverStudy.Data;
 
 namespace WebAspDBeaverStudy.Controllers
 {
     public class MainController : Controller
     {
+        private readonly AppDbContext _dbContext;
+
+        //DI - Depencecy Injection
+        public MainController(AppDbContext context)
+        {
+            _dbContext = context;
+        }
+
         // Метод у контролері називається - action - дія
         public IActionResult Index() //IActionResult - для html сторінки
         {
-            return View();
+            var model = _dbContext.Categories.ToList();
+            return View(model);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using WebAspDBeaverStudy.Data;
+using WebAspDBeaverStudy.Data.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("MyConectionDB")));
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -19,5 +26,39 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Main}/{action=Index}/{id?}");
+
+using (var serviceScope = app.Services.CreateScope())
+{
+    var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+    context.Database.Migrate();
+
+    if (!context.Categories.Any())
+    {
+        var sausage = new CategoryEntity
+        {
+            Name = "Sausage",
+            Image = "https://rivnepost.rv.ua/img/650/korisnoi-kovbasi-ne-buvae-hastroenterolohi-nazvali_20240612_4163.jpg",
+            Description = "Тим часом відмовлятися від ковбаси повністю не обов’язково. " +
+            "Важливо пам’ятати, що це делікатес, який можна вживати не більше 50 грамів на день."
+        };
+
+        var cheese = new CategoryEntity
+        {
+            Name = "Cheese",
+            Image = "https://www.vsesmak.com.ua/sites/default/files/styles/large/public/field/image/syrnaya_gora_5330_1900_21.jpg?itok=RPUrRskl",
+            Description = "Cир – один з найчастіших гостей на нашому столі. " +
+            "Адже це і смачно, і корисно, і доступно. Не можна сказати, що увесь, " +
+            "що продається на прилавках супермаркетів твердий сир – неякісний."
+        };
+
+        var bread = new CategoryEntity
+        {
+            Name = "Bread",
+            Image = "https://mind.ua/openmind/20206794-hlibne-pitannya-pro-mifi-ta-korist-bezdrizhdzhovogo-hliba",
+            Description = "У сегменті ринку «здорового харчування» існують сорти хліба, " +
+            "які майже не сприяють набору зайвої ваги – наприклад, цільнозерновий хліб."
+        };
+    }
+}
 
 app.Run();
