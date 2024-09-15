9a41d277f70ac57082e2fa0f16bcf89ecbc2b7fb
Shoro749 <romanbro749@gmail.com>
2024-09-15 13:36:02 +0300

    Add project files.

---
+++ Controllers/MainController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAspDBeaverStudy.Controllers
+{
+    public class MainController : Controller
+    {
+        // Метод у контролері називається - action - дія
+        public IActionResult Index() //IActionResult - для html сторінки
+        {
+            return View();
+        }
+    }
+}
