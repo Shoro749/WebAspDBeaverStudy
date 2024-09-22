6668347793a2a7a58447988140de4499f2139126
Shoro749 <romanbro749@gmail.com>
2024-09-22 16:20:09 +0300

    Commit 2.1

--- Controllers/MainController.cs
+++ Controllers/MainController.cs
@@ -1,22 +1,56 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebAspDBeaverStudy.Data;
+using WebAspDBeaverStudy.Models.Category;
 
 namespace WebAspDBeaverStudy.Controllers
 {
     public class MainController : Controller
     {
         private readonly AppDbContext _dbContext;
+        private readonly IWebHostEnvironment _environment;
 
         //DI - Depencecy Injection
-        public MainController(AppDbContext context)
+        public MainController(AppDbContext context,
+            IWebHostEnvironment environment)
         {
             _dbContext = context;
+            _environment = environment;
         }
 
         // Метод у контролері називається - action - дія
         public IActionResult Index() //IActionResult - для html сторінки
         {
             var model = _dbContext.Categories.ToList();
+            return View(model);
+        }
+
+        [HttpGet] 
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost] // Отримуємо дані із форми 
+        public IActionResult Create(CategoryCreateViewModel model)
+        {
+            // Збережемо в базу даних інформації
+            var dirName = "uploading";
+            var dirSave = Path.Combine(_environment.WebRootPath, dirName);  
+            if (!Directory.Exists(dirSave))
+            {
+                Directory.CreateDirectory(dirSave);
+            }
+            if (model.Photo != null)
+            {
+                // Унікальне значення (неповторне)
+                string fileName = Guid.NewGuid().ToString();
+                var ext = Path.GetExtension(model.Photo.FileName);
+                fileName += ext;
+                var saveFile = Path.Combine(dirSave, fileName);
+                using (var stream = new FileStream(saveFile, FileMode.Create))
+                    model.Photo.CopyTo(stream);
+            }
+            // Що отримали те й повертаємо назад
             return View(model);
         }
     }
---
+++ Models/Category/CategoryCreateViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAspDBeaverStudy.Models.Category
+{
+    public class CategoryCreateViewModel
+    {
+        [Display(Name = "Назва категорії")]
+        public string Name { get; set; } = String.Empty;
+        [Display(Name = "Оберіть фото на ПК")]
+        public IFormFile? Photo { get; set; }
+        [Display(Name = "Короткий опис")]
+        public string Description { get; set; } = string.Empty;
+    }
+}
